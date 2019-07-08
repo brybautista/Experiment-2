4e5a47509c0c60cea36f35a77ed9ed87c4cd9782
brybautista <johnbryan.bautista.eng@ust.edu.ph>
2019-07-08 21:46:27 +0800

    fourth

---
+++ EXPERIMENT2_PROBLEM4.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main() 
+
+{
+	int x, y;
+
+	
+	
+	for ( x = 1; x <= 9; x++)
+	
+	{
+		cout << x << " , "; 
+		
+	}
+	
+	
+	
+	for ( x = 10; x <= 30; x += 2)
+	
+	{
+		cout << x << " , ";
+	}
+	
+	
+	
+	getch();
+	return 0;
+}
